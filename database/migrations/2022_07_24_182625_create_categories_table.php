911c32cbd71747c8d9a7796b76c9717dabd63671
DarknessNinjaBR <darkenessninjabr@hotmail.com>
2022-07-24 15:49:01 -0300

    chore: color column in migration table

--- database/migrations/2022_07_24_182625_create_categories_table.php
+++ database/migrations/2022_07_24_182625_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 12);
+            $table->string('color', 6);
             $table->bigInteger('user_id')->foreign('user_id')->references('id')->on('users');
             $table->softDeletes();
             $table->timestamps();
